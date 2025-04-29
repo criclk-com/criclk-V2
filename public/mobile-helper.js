0a16ec5a0f8fa3cdf95828910184a7d588fb5872
cyberdevlk <138901572+cyberdevlk@users.noreply.github.com>
2025-04-30 04:58:10 +0530

    Create mobile-helper.js

---
+++ public/mobile-helper.js
@@ -0,0 +1,7 @@
+/**
+ * Mobile Helper Script for Cricket Streaming
+ * This script helps mobile devices play streams in desktop mode
+ */
+
+// Detect if running on a mobile device
+const isMobile = /Android|webOS|iPhone|iPad|iPod|Black
